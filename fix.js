bdee32c3679a3e24e4852e613331a4f5403baa69
eggnogg <31466851+4ssHole@users.noreply.github.com>
2022-03-22 03:26:44 +0800

    1

---
+++ fix.js
@@ -0,0 +1,2 @@
+document.querySelector("input[id='Frm_Username']").setAttribute("autocomplete", "username");
+document.querySelector("input[id='Frm_Password']").setAttribute("autocomplete", "password");
