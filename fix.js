09ddb5cc43ac357645281e155912d7fb8c366d6e
4ssh0le <juanpaologillera@gmail.com>
2022-03-22 03:38:40 +0800

    3

--- fix.js
+++ fix.js
@@ -1,4 +1,5 @@
+fix.js application/javascript
 (function() {
     document.querySelector("input[id='Frm_Username']").setAttribute("autocomplete", "username");
     document.querySelector("input[id='Frm_Password']").setAttribute("autocomplete", "password");
-  }());
+}());
